e5d5f9c088acb7a77aeb35c62b9d6785934eff9a
meequz <meequz@gmail.com>
2014-09-22 18:35:09 +0300

    Fix resolution vars in example

--- example_randomcolors.py
+++ example_randomcolors.py
@@ -3,9 +3,10 @@
 import pixelqt
 import numpy
 
+w, h = 120, 90
+
 
 def get_imdata():
-	w, h = 400, 300
 	pic = numpy.random.random((w,h,3)) * 255
 	pic8 = numpy.uint8(pic)
 	return pic8
@@ -13,8 +14,8 @@ def get_imdata():
 
 mygame = pixelqt.Game(draw_func=get_imdata)
 mygame.config['name'] = 'Random colors'
-mygame.config['w'] = 120
-mygame.config['h'] = 90
+mygame.config['w'] = w
+mygame.config['h'] = h
 mygame.config['gl'] = False
 mygame.init_controls('resolution', 'zoom')
 mygame.run()
