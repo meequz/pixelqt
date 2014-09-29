48f66686394bc17abe67ed8957e58aa20854903d
meequz <meequz@gmail.com>
2014-09-29 17:27:08 +0300

    Add explaining comments to an example

--- example_randomcolors.py
+++ example_randomcolors.py
@@ -4,6 +4,8 @@ import pixelqt
 from random import randint
 
 
+# Function that returns dict if format: {(coordinates): (rgb color)}
+# Other areas will be filled by background color.
 def get_imdata(w, h, frame_count):
     res = {}
     for line in range(h):
@@ -13,12 +15,20 @@ def get_imdata(w, h, frame_count):
     return res
 
 
+# Create game and specify function that will be called periodically.
 mygame = pixelqt.Game(draw_func=get_imdata)
+
+# Set your own default options. No one is required.
+# If you don't set them, the defaults will be used.
 mygame.config['name'] = 'Almost random colors'
 mygame.config['w'] = 120
 mygame.config['h'] = 90
 mygame.config['background'] = (255, 255, 150)
 mygame.config['gl'] = True
 mygame.config['save_each'] = 17
+
+# Add controls which you want to see in game's window.
 mygame.init_controls('resolution', 'zoom', 'background', 'gl')
+
+# Run the simulation.
 mygame.run()
